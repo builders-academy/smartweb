dcf0fd154ca66acad6a737f8febc96f4151256b6
Biwas Bhandari <biwas2059@gmail.com>
2024-08-24 10:17:02 +0545

    fix(api): fixed api call

--- src/helpers/connect.ts
+++ src/helpers/connect.ts
@@ -56,7 +56,7 @@ export const useConnectWalletSats = () => {
       if (stackAddress) {
         const res = await fetch(
           // `https://api.mainnet.hiro.so/v2/accounts/${stackAddress}`
-          "https://api.hiro.so/extended/v1/address/SP15GXS91AWJ4X5ZQNNE4DQ94MWVFMD3E6AA1SM36/balances"
+          `https://api.hiro.so/extended/v1/address/${stackAddress}/balances`
         );
         if (res.ok) {
           const data = await res.json();
